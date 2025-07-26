05c64baf843055f24b7c0a02060fa9e793a08fb1
jctinin <jctinin@outlook.com>
2025-07-26 18:31:36 -0300

    explicando o factory

--- src/main/java/com/designpatternsex/examples/factory/CleaningRoboFactory.java
+++ src/main/java/com/designpatternsex/examples/factory/CleaningRoboFactory.java
@@ -4,6 +4,13 @@ public class CleaningRoboFactory extends RoboFactory {
 
   @Override
   public Robo createRobo() {
+    // Este método cria uma instância de CleaningRobo
+    // e a retorna.
+    // Isso permite que a fábrica crie robôs de limpeza.
+    // O método work() será chamado quando newRobo() for invocado.
+    // Isso demonstra o padrão de projeto Factory Method.
+    // O Factory Method permite que subclasses decidam qual classe instanciar.
+    // Neste caso, a fábrica cria um robô de limpeza.
     return new CleaningRobo();
   }
 }
--- src/main/java/com/designpatternsex/examples/factory/RoboFactory.java
+++ src/main/java/com/designpatternsex/examples/factory/RoboFactory.java
@@ -2,10 +2,21 @@ package com.designpatternsex.examples.factory;
 
 public abstract class RoboFactory {
 
+  // Quando uma subclasse sobrescreve este método, ela o sobrescreve retornando uma instância do Robo
+  // do tipo que ela deseja criar. Isso permite que a fábrica crie diferentes tipos de robôs
+  // ou seja o método createRobo() passa a retornar um CookingRobo, CleaningRobo ou SpeakingRobo
+  // dependendo da subclasse que o implementa.
+
   public abstract Robo createRobo();
 
   public void newRobo() {
+
+    // Ou seja: este método createRobo(), já não é mais o createRobo() da superclasse,
+    // mas sim o createRobo() da subclasse que o implementa(CookingRoboFactory, CleaningRoboFactory, SpeakingRoboFactory).
+    // Isso demonstra o padrão de projeto Factory Method.
     Robo createdRobo = createRobo();
+
+
     createdRobo.work();
   }
 
